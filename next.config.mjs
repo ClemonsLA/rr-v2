d0315441127ce1d880e701d7b48e52412b6b8469
ClemonsLA <113805041+ClemonsLA@users.noreply.github.com>
2024-02-25 02:38:04 +0000

    more nextjs config commits

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
           allowedOrigins: [
             'localhost:3000',
             'rr-v2.vercel.app',
+            'https://rr-v2.vercel.app',
             'solid-space-telegram-4j7xw57jjj7hqwv-3000.app.github.dev',
           ]
         }
